d2ca0563887ebae29c811a5a3fd8cbeba86f437a
LemuDev <lemu200011@gmail.com>
2024-05-03 15:08:42 -0300

    inial commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          
+          // logo
+          Icon(
+          Icons.message,
+          size: 60,
+          color: Theme.of(context).colorScheme.primary,
+          ),
+          // welcome back message
+          Text(
+            "Welcome back, you've been missed!",
+             style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 16,
+            ),
+           ),
+          // email textfield
+          TextField(),
+          // pw texfield
+
+          // login button
+
+          // register now
+
+        ],
+      ),
+      ),
+    );
+  }
+}
