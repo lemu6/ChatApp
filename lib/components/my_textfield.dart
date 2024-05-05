1ec1aa68380f2247b5eaa9514239872265bcfec8
LemuDev <lemu200011@gmail.com>
2024-05-05 15:18:49 -0300

    button

--- lib/components/my_textfield.dart
+++ lib/components/my_textfield.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class MyTextField extends StatelessWidget {
   final String hintText;
   final bool obscureText;
+  final TextEditingController controller;
 
   const MyTextField({
     super.key, 
     required this.hintText,
     required this.obscureText,
+    required this.controller,
   });
 
   @override
@@ -16,6 +18,7 @@ class MyTextField extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 25.0),
       child: TextField(
         obscureText: obscureText,
+        controller: controller,
         decoration: InputDecoration(
           enabledBorder: OutlineInputBorder(
             borderSide:
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,18 @@
+import 'package:chat_app/components/my_button.dart';
 import 'package:chat_app/components/my_textfield.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  //email and pw text controllers
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+
+  LoginPage({super.key});
+
+  // login method
+  void login() {
+    
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,43 +20,50 @@ class LoginPage extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Center(
         child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          
-          // logo
-          Icon(
-          Icons.message,
-          size: 60,
-          color: Theme.of(context).colorScheme.primary,
-          ),
-          // welcome back message
-          Text(
-            "Welcome back, you've been missed!",
-             style: TextStyle(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Icon(
+              Icons.message,
+              size: 60,
               color: Theme.of(context).colorScheme.primary,
-              fontSize: 16,
             ),
-           ),
-          // email textfield
-          MyTextField(
-            hintText: "Email",
-            obscureText: false,
-          ),
+            // welcome back message
+            Text(
+              "Welcome back, you've been missed!",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+            // email textfield
+            MyTextField(
+              hintText: "Email",
+              obscureText: false,
+              controller: _emailController,
+            ),
 
-          const SizedBox(height: 10),
+            const SizedBox(height: 10),
 
-          // pw texfield
-          MyTextField(
-            hintText: "Password",
-            obscureText: true,
-          ),
+            // pw texfield
+            MyTextField(
+              hintText: "Password",
+              obscureText: true,
+              controller: _pwController,
+            ),
 
-          // login button
+            const SizedBox(height: 25),
 
-          // register now
+            // login button
+            MyButton(
+              text: "Login",
+              onTap: login,
+               
+            ),
 
-        ],
-      ),
+            // register now
+          ],
+        ),
       ),
     );
   }
