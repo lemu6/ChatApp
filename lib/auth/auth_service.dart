ad9b9741200458201a094e28fceeea9a3966825a
LemuDev <lemu200011@gmail.com>
2024-05-10 22:29:21 -0300

    logout

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -19,7 +19,24 @@ class AuthService {
 
   // sign up
 
-  // sign out
+  Future<UserCredential> signUpWithEmailPassword(String email, password) async {
+    try {
+      UserCredential userCredential =
+        await _auth.createUserWithEmailAndPassword(
+          email: email, 
+          password: password,
+          );
 
+          return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+      } 
+    }
+  }
+
+  // sign out
+  Future<void> signOut() async {
+    return await _auth.signOut();
+  }
   // errors
 }
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,5 +1,6 @@
+import 'package:chat_app/auth/auth_service.dart';
 import 'package:flutter/material.dart';
-import 'package:chat_app/components/my_button.dart'; 
+import 'package:chat_app/components/my_button.dart';
 import 'package:chat_app/components/my_textfield.dart';
 
 class RegisterPage extends StatelessWidget {
@@ -7,12 +8,28 @@ class RegisterPage extends StatelessWidget {
   final TextEditingController _pwController = TextEditingController();
   final TextEditingController _confirmPwController = TextEditingController();
 
-    final void Function()? onTap;
+  final void Function()? onTap;
 
   RegisterPage({super.key, required this.onTap});
 
-  //register method 
+  //register method
   void register() {}
+    // get auth service
+    final _auth = AuthService();
+
+    if (_pwController.text == _confirmPwController.text){
+      try {
+        _auth.signUpWithEmailPassword(
+      _emailController.text, 
+      _pwController.text,
+        );
+    } catch (e) {
+
+
+        }
+      }
+    }
+  
 
   @override
   Widget build(BuildContext context) {
