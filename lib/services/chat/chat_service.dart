2e7708a7a0ba9b0b301bff4fff86dd02829ef0db
LemuDev <lemu200011@gmail.com>
2024-05-15 00:28:39 -0300

    database

---
+++ lib/services/chat/chat_service.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatService {
+  // get instance of firestore
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  // get user stream
+
+  Stream<List<Map<String,dynamic>> getUsersStream() {
+
+  }
+  // send message
+
+  // get message
+}
